3268bc53d659e777fe81a3fe27c6d7562b41166f
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-12-20 07:05:04 -0800

    checks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-    import sys
     try:
-        return(fct(*args))
-    except:
-        sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
-        return(None)
+        result = fct(*args)
+        return result
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
